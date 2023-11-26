2521a1ac89010c948a8dec208534e21046b3bc8a
Kevin Segaud <kevin@sakemaer.fr>
2023-11-26 10:54:26 +0100

    fix exported url for io and web

--- lib/x_key_value.dart
+++ lib/x_key_value.dart
@@ -1,5 +1,5 @@
 export 'package:x_key_value/src/factory.dart';
 export 'package:x_key_value/src/type.dart';
 export 'package:x_key_value/src/base.dart'
-    if (dart.library.io) 'package:x_key_value/src/storage_io.dart'
-    if (dart.library.js) 'package:x_key_value/src/storage_web.dart';
+    if (dart.library.io) 'package:x_key_value/src/io.dart'
+    if (dart.library.js) 'package:x_key_value/src/web.dart';
